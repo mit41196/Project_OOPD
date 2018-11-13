a16584bd3e06ba6b0a4c1b85c87ea93fe1698d95
msahni825 <42293547+msahni825@users.noreply.github.com>
2018-11-14 00:12:57 +0530

    File Updated

--- src/FindByNamePage.java
+++ src/FindByNamePage.java
@@ -21,11 +21,7 @@ public class FindByNamePage extends JFrame {
 	
 	private JPanel contentPane;
 	DefaultListModel model;
-	
-	/**
-	 * Launch the application.
-	 */
-	JComboBox comboBox = new JComboBox();
+	private JTextField textField;
 	
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
@@ -43,30 +39,30 @@ public class FindByNamePage extends JFrame {
 	/**
 	 * Create the frame.
 	 */
-	private void fillComboBoxId()
-	{
-		try 
-		{
-			Class.forName("com.mysql.jdbc.Driver");
-		
-			Connection c=DriverManager.getConnection("jdbc:mysql://localhost/oopd","root","root");
-			Statement st=c.createStatement();
-			ResultSet rs=st.executeQuery("Select * from doctor");
-			while(rs.next())
-			{
-				String name = rs.getString("name");
-				comboBox.addItem(name);
-			}
-			System.out.println("query");
-		}
-		catch(Exception e1)
-		{
-			e1.printStackTrace();
-		}
-	}
+//	private void fillComboBoxId()
+//	{
+//		try 
+//		{
+//			Class.forName("com.mysql.jdbc.Driver");
+//		
+//			Connection c=DriverManager.getConnection("jdbc:mysql://localhost/oopd","root","root");
+//			Statement st=c.createStatement();
+//			ResultSet rs=st.executeQuery("Select * from doctor");
+//			while(rs.next())
+//			{
+//				String name = rs.getString("name");
+//				//comboBox.addItem(name);
+//			}
+//			System.out.println("query");
+//		}
+//		catch(Exception e1)
+//		{
+//			e1.printStackTrace();
+//		}
+//	}
 
 	public FindByNamePage() {
-		fillComboBoxId();
+		//fillComboBoxId();
 		model = new DefaultListModel();
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
@@ -75,14 +71,7 @@ public class FindByNamePage extends JFrame {
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		System.out.println("vcx");
-		
-		comboBox.setBounds(190, 79, 185, 20);
-		
-
-		contentPane.add(comboBox);
-		
-		String id = (String)comboBox.getSelectedItem();
-		System.out.println(id);
+		//System.out.println(id);
 		
 		JLabel label = new JLabel("");
 		label.setBounds(22, 131, 46, 14);
@@ -90,7 +79,7 @@ public class FindByNamePage extends JFrame {
 		
 		JButton btnView = new JButton("VIEW");
 		btnView.setFont(new Font("Tahoma", Font.BOLD, 12));
-		btnView.setBounds(173, 145, 89, 23);
+		btnView.setBounds(139, 88, 89, 46);
 		contentPane.add(btnView);
 		
 		JLabel lblSelectDrBy = new JLabel("Select DR. by NAME:");
@@ -98,10 +87,10 @@ public class FindByNamePage extends JFrame {
 		lblSelectDrBy.setBounds(49, 11, 202, 28);
 		contentPane.add(lblSelectDrBy);
 		
-		JLabel lblNameOfDr = new JLabel("NAME OF DR.");
-		lblNameOfDr.setFont(new Font("Tahoma", Font.BOLD, 12));
-		lblNameOfDr.setBounds(49, 82, 102, 17);
-		contentPane.add(lblNameOfDr);
+		textField = new JTextField();
+		textField.setBounds(203, 16, 86, 20);
+		contentPane.add(textField);
+		textField.setColumns(10);
 
 			}
 }
